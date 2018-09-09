25b447909d44e6506d19d35245b8fee07d41b2b7
Abe Beckwith <loubecwith@gmail.com>
2018-09-09 11:52:45 -0400

    job postings

--- src/components/jobs.js
+++ src/components/jobs.js
@@ -1,5 +1,7 @@
 import React, { Component, ReactComponent } from 'react';
 import './common/header.css';
+import location from './job_location.png'
+import arrow from './arrow.png'
 import {Header, Footer} from './common/header.js'
 
 
@@ -10,8 +12,54 @@ class Jobs extends Component {
         <div className="blog-header" >
         Career Opportunities 
         </div>
-        <div className="no-posts">
+        <div className="no-posts" hidden={true}>
         Check back soon!
+        </div>
+        <div className="jobs-div">
+          <Job title="Hack Cooper Hackathon" 
+          desc="Hack Cooper is a 24 Hour hackathon located at Cooper Union for graduate and undergraduate students,
+         artists, designers, and hardware tinkerers. Build your dream project!"  
+          link="http://hackcooper.org/" 
+          date="Date: October 12-13, 2018" 
+          loc="Cooper Union, NY"/>
+        </div>
+      </div>
+    );
+  } 
+}
+
+class Job extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className="job" >
+        <div className="job-column left">
+          <img src={location} className="job-loc-img"/>
+          <div className="job-location">
+            {this.props.loc}
+          </div>
+        </div>
+        <div className="job-column middle">
+          <div className="job-item job-title">
+            {this.props.title}
+          </div>
+          <div className="job-item job-date">
+            {this.props.date}
+          </div>
+          
+          <div className="job-item job-description" >
+            {this.props.desc}
+          </div> 
+        </div>
+        <div className="job-column right">
+          <div className="job-item job-link">
+            <a className="button job-link-btn" href={this.props.link} target="_blank"> 
+              More Info 
+            </a>
+          </div>
         </div>
       </div>
     );
