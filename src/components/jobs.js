cf79186300a32c103dd63bbbec4aa95ac90974c0
rtkgupta <gupta.ritika@husky.neu.edu>
2018-11-21 17:49:10 -0500

    test commit

--- src/components/jobs.js
+++ src/components/jobs.js
@@ -10,39 +10,56 @@ class Jobs extends Component {
     return (
       <div className="App">
         <div className="jobs-header" >
-        Career Opportunities 
+        Career Opportunities
         </div>
         <div className="no-posts" hidden={true}>
         Check back soon!
         </div>
         <div className="jobs-div">
-          <Job title="Hack Cooper Hackathon" 
+          <Job title="Hack Cooper Hackathon"
           desc="Hack Cooper is a 24 Hour hackathon located at Cooper Union for graduate and undergraduate students,
-         artists, designers, and hardware tinkerers. Build your dream project!"  
-          link="http://hackcooper.org/" 
-          date="Date: October 12-13, 2018" 
+         artists, designers, and hardware tinkerers. Build your dream project!"
+          link="http://hackcooper.org/"
+          date="Date: October 12-13, 2018"
           loc="Cooper Union, NY"/>
         </div>
         <div className="jobs-div">
-          <Job title="Digital Technology Leadership Program (DTLP) - 2019" 
-          desc="The Digital Technology Leadership Program (DTLP) offers those with a passion 
-          to grow their leadership, business acumen, and technical skills while building a career 
-          with unlimited potential. Program members lead by doing, gaining real-world experience by 
-          working on important, challenging projects alongside some of the brightest technical minds 
-          in the world." 
-          link="https://jobs.gecareers.com/ShowJob/Id/59662/Digital-Technology-Leadership-Program-(DTLP)-2019/" 
-          date="Date: October 12-13, 2018" 
-          loc="United States; Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, 
-          Delaware, District of Columbia, Florida, Georgia, Idaho, Illinois, Indiana, Iowa, Kansas, 
-          Kentucky, Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, 
-          Missouri, Montana, Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, 
-          North Carolina, North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, 
-          South Carolina, South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, 
+          <Job title="Digital Technology Leadership Program (DTLP) - 2019"
+          desc="The Digital Technology Leadership Program (DTLP) offers those with a passion
+          to grow their leadership, business acumen, and technical skills while building a career
+          with unlimited potential. Program members lead by doing, gaining real-world experience by
+          working on important, challenging projects alongside some of the brightest technical minds
+          in the world."
+          link="https://jobs.gecareers.com/ShowJob/Id/59662/Digital-Technology-Leadership-Program-(DTLP)-2019/"
+          date="Date: October 12-13, 2018"
+          loc="United States; Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut,
+          Delaware, District of Columbia, Florida, Georgia, Idaho, Illinois, Indiana, Iowa, Kansas,
+          Kentucky, Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi,
+          Missouri, Montana, Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York,
+          North Carolina, North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island,
+          South Carolina, South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington,
+          West Virginia, Wisconsin, Wyoming"/>
+        </div>
+        <div className="jobs-div">
+          <Job title="Digital Technology Leadership Program (DTLP) - 2019-----11111" 
+          desc="The Digital Technology Leadership Program (DTLP) offers those with a passion
+          to grow their leadership, business acumen, and technical skills while building a career
+          with unlimited potential. Program members lead by doing, gaining real-world experience by
+          working on important, challenging projects alongside some of the brightest technical minds
+          in the world."
+          link="https://jobs.gecareers.com/ShowJob/Id/59662/Digital-Technology-Leadership-Program-(DTLP)-2019/"
+          date="Date: October 12-13, 2018"
+          loc="United States; Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut,
+          Delaware, District of Columbia, Florida, Georgia, Idaho, Illinois, Indiana, Iowa, Kansas,
+          Kentucky, Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi,
+          Missouri, Montana, Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York,
+          North Carolina, North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island,
+          South Carolina, South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington,
           West Virginia, Wisconsin, Wyoming"/>
         </div>
       </div>
     );
-  } 
+  }
 }
 
 class Job extends Component {
@@ -66,15 +83,15 @@ class Job extends Component {
           <div className="job-item job-date">
             {this.props.date}
           </div>
-          
+
           <div className="job-item job-description" >
             {this.props.desc}
-          </div> 
+          </div>
         </div>
         <div className="job-column right">
           <div className="job-item job-link">
-            <a className="button job-link-btn" href={this.props.link} target="_blank"> 
-              More Info 
+            <a className="button job-link-btn" href={this.props.link} target="_blank">
+              More Info
             </a>
           </div>
         </div>
